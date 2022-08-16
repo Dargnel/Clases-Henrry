ff4aaba0366f0793ad1bac0bcc596c4fa332f5df
Iván García <hectorivang2000@gmail.com>
2022-08-16 12:18:07 -0500

    Homework intro

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,10 +3,28 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  var reves= num.split("").reverse().join("");
+  var resultado = 0;
+
+  for (var i = 0;i < reves.length;i++){
+    resultado = resultado +( reves.charAt(i)* (Math.pow(2,i)) )
+
+  }
+  return resultado;
+
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+
+  var resultado = '';
+  while (num >= 0.5){
+    resultado =resultado + (num % 2);
+    num = Math.trunc(num/2);
+  }
+  return resultado.split("").reverse().join("");;
+
 
 }
 
